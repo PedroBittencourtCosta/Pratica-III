2c90578e53b04630758807de96ad29348ac66998
PedroBittencourtCosta <Pedro.Bittencourt.dev@gmail.com>
2023-08-20 17:31:26 -0300

    praticando collections

---
+++ collections/list/operacoesBasicas/Tarefa.java
@@ -0,0 +1,14 @@
+package collections.list.operacoesBasicas;
+
+public class Tarefa {
+
+    private String descricao;
+
+    public Tarefa(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
